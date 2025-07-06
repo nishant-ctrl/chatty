1bfe004744fe766bfc777cbb81d0438912d46ae2
nishant-ctrl <knishant9135@gmail.com>
2025-07-06 23:05:12 +0530

    prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,21 +2,21 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 import setupSocket from "../socket.js";
-import {createServer} from "http"
+import { createServer } from "http";
 import path from "path";
 const __dirname = path.resolve();
-
+import express from "express";
 dotenv.config({
     path: "./.env",
 });
 const port = process.env.PORT || 7272;
 
-const server = createServer(app)
+const server = createServer(app);
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 
@@ -30,7 +30,6 @@ connectDB()
             console.error("Server Error:", error);
             throw error;
         });
-        
     })
     .catch((err) => {
         console.log("MONGO db connecton failed : ", err);
